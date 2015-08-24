b19df19b805cc3b9f257ca02447787f418dd8f36
kinglemuel <pro@kinglemuel.com>
2015-08-24 00:33:24 -0400

    update styles

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,38 +4,46 @@ require 'open-uri'
 
 class StudentsController < ApplicationController
   def index
-    @students = Student.all 
+    all_profiles if Student.all.empty?
+    @students = Student.all.sort_by { |stu| stu.score }
   end
 
-  def show
-    @student = Student.find(params[:id])
-  end
+  # def show
+  #   @student = Student.find(params[:id])
+  # end
 
-  def new
-    @student = Student.new
-    get_profile(@student.username)
-  end
+  # def new
+  #   @student = Student.new
+  #   get_profile(@student.username)
+  # end
 
-  def create
-  end
+  # def create
+  # end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
 
   private #explain why these methods were made private
 
   # Explain the params addition 
-  def student_params
-    params.require(:student).permit(:name, :username, :score)
-  end
+  # def student_params
+  #   params.require(:student).permit(:name, :username, :score)
+  # end
 
   # Get data from learn per user
+  def all_profiles
+    students = ["sdolmo", "Bmesa620", "BrunaNett", "Cranium1", "DJoseph1250", "EstherMo", "GxDesign", "Ilapides", "JaimieWalker", "KingLemuel", "LewisMatos", "Omrika", "PurpIeHaze", "RaptorDog", "Samueljoli", "ZalmanB", "akpersad", "cloudleo", "damianlajara", "dylan-okeefe", "feljen", "gil93", "henrypl95", "hnae6443", "jmdelvalle", "lawrencechong", "peterhan92", "skoltz", "techsin", "wolfwzrd", "mannybeso", "Gettekt"] 
+
+    # students = [  "KingLemuel", "LewisMatos", "Omrika", "PurpIeHaze", "RaptorDog", "damianlajara", "dylan-okeefe",  "hnae6443", "jmdelvalle", "lawrencechong", "peterhan92", "skoltz", "techsin", "wolfwzrd", "mannybeso", "Gettekt"] 
+    students.each { |student| get_profile(student) } 
+  end
+
   def get_profile(username)
-    doc = Nokogiri::HTML(open("https://learn.co/#{student}", 'Accept' => 'text/html'))
+    doc = Nokogiri::HTML(open("https://learn.co/#{username}", 'Accept' => 'text/html'))
     name = doc.css('.row.user-name span.h3.title').text
     score = doc.css('div[data-track-id="1564"] .col-sm-3.lessons-complete-container h3').text.to_i
     Student.create(name: name, username: username, score: score)
